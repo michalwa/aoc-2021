f3efaf7d727891482fc60ec8c56d9395e64518c6
michalwa <michalwa2003@gmail.com>
2021-12-06 11:56:14 +0100

    Tweak input fetching

--- fetch_input.py
+++ fetch_input.py
@@ -15,17 +15,18 @@ DEFAULT_OUTPUT = "inputs/day{day}{suffix}.txt"
 parser = argparse.ArgumentParser(description="Fetches input from AoC")
 parser.add_argument("-y", "--year", required=True, type=int)
 parser.add_argument("-d", "--day", required=True, type=int)
+parser.add_argument("-i", "--input", action="store_true",
+                    help="fetches the actual input")
 parser.add_argument("-e", "--example", action="store_true",
                     help="fetches the example instead of actual input")
 parser.add_argument("-o", "--output", type=argparse.FileType("w"),
                     help="specifies the output file")
 
 
-def write_output(output: str, args: argparse.Namespace) -> None:
+def write_output(output: str, args: argparse.Namespace, suffix: str = '') -> None:
     if args.output:
         args.output.write(output)
     else:
-        suffix = '-example' if args.example else ''
         filename = DEFAULT_OUTPUT.format(suffix=suffix, **args.__dict__)
 
         with open(filename, "w", encoding="utf-8") as f:
@@ -36,7 +37,7 @@ if __name__ == "__main__":
     load_dotenv()
     args = parser.parse_args()
 
-    if not args.example:
+    if args.input:
         response = requests.get(
             INPUT_URL.format(**args.__dict__),
             cookies={"session": os.getenv("AOC_SESSION")},
@@ -48,7 +49,7 @@ if __name__ == "__main__":
 
         write_output(response.text, args)
 
-    else:
+    if args.example:
         response = requests.get(EXAMPLE_URL.format(**args.__dict__))
 
         if not response.ok:
@@ -56,7 +57,7 @@ if __name__ == "__main__":
             exit(1)
 
         bs = BeautifulSoup(response.text, features="html.parser")
-        # The example is usually contained within the first <code> element
-        content = bs.find("code").text
+        # The example is usually contained within the first <pre> element
+        content = bs.select_one("pre > code").text
 
-        write_output(content, args)
+        write_output(content + '\n', args, '-example')
